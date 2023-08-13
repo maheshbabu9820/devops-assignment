dec6c0c24621b2573c629a60fef56d401dbc4d3c
maheshbabu9820 <89741621+maheshbabu9820@users.noreply.github.com>
2023-08-13 14:32:11 +0530

    Update nft-bridge.spec.ts

--- apps/nft-bridge-e2e/src/nft-bridge/nft-bridge.spec.ts
+++ apps/nft-bridge-e2e/src/nft-bridge/nft-bridge.spec.ts
@@ -1,10 +1,25 @@
 import axios from 'axios';
 
+// Mock Axios
+jest.mock('axios');
+
 describe('GET /', () => {
   it('should return a message', async () => {
+    // Mock the Axios GET request
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: { message: 'Hello API' }
+    });
+
+    // Your test code
     const res = await axios.get(`/`);
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
+
+
+
+
+
